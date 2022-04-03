2043f167e3b3eda946cfaedf44193ce92a6f7759
pavelpminaev <pavelpminaev@gmail.com>
2022-04-03 17:39:38 +0300

    step 5.3

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-
-import prompt
+"""Docstr for you."""
 import random
+import prompt
 
 
 def main():
@@ -11,20 +11,20 @@ def main():
     user_name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(user_name))
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i <= 2:
-        random_num = random.randint(1, 100) 
+    index = 0
+    while index <= 2:
+        random_num = random.randint(1, 100)
         print('Question: ' + str(random_num))
-        answer = prompt.string('Your answer: ')
+        swr = prompt.string('Your answer: ')
         if random_num % 2 == 0:
-            true_answer = 'yes'
+            true_swr = 'yes'
         else:
-            true_answer = 'no'
-        if answer == true_answer:
+            true_swr = 'no'
+        if swr == true_swr:
             print('Correct!')
-            i = i + 1
+            index = index + 1
         else:
-            print(answer + ' is wrong answer ;(. Correct answer was ' + true_answer)
+            print(swr + ' is wrong answer ;(. Correct answer was ' + true_swr)
             print("Let's try again, {0}!".format(user_name))
     print('Congratulations, {0}!'.format(user_name))
 
