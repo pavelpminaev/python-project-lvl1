6109385cc10af962647ae4fa1e20932ac21dcfe7
pavelpminaev <pavelpminaev@gmail.com>
2022-05-16 23:04:04 +0300

    def is_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,20 +8,30 @@ from random import randint
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(number):
+    """Function defines prime numbers"""
+
+    if number < 2:
+        return False
+    else:
+        divider = 2
+        while divider <= number / 2:
+            if number % divider == 0:
+                return False
+            divider += 1
+        return True
 
 
-
-def answer_generator():
-    """Function ask question and return correct_answer"""
+def generate_question_answer():
+    """Function generate question and correct_answer"""
 
     number = randint(0, 100)
 
-    print(f'Question: {number}')
-    if number < 2:
-        return 'no'
-    divider = 2
-    while divider <= number / 2:
-        if number % divider == 0:
-            return 'no'
-        divider += 1
-    return 'yes'
+    if is_prime(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+
+    question = f'Question: {number}'
+
+    return question, correct_answer
